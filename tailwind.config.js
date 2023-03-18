442dd029b279d65024080baead3cdec1e8cffa73
Anthony Destenay <anthony.destenay@gmail.com>
2023-03-18 18:19:27 +0100

    Added DaisyUI + theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        thoanny: {
+          "primary": "#93C045",
+          "secondary": "#81AA39",
+          "accent": "#344417",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
 }
