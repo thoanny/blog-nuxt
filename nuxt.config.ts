f8b3bf881faf6b93bf1538418f2a96a90193df24
Anthony Destenay <anthony.destenay@gmail.com>
2023-03-21 09:18:18 +0100

    Added favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 const seoTitle = 'Thoanny';
 const seoDescription = 'Blog de Thoanny, où sont partagés des actualités de ses projets personnels, des articles sur le thème du streaming, du jeu vidéo, et aussi des trucs et astuces...';
-const seoImage = 'TODO';
+const seoImage = 'https://wp.thoanny.fr/wp-content/uploads/preview.png';
 
 export default defineNuxtConfig({
     postcss: {
@@ -26,6 +26,14 @@ export default defineNuxtConfig({
                 { property: 'twitter:description', content: seoDescription },
                 { property: 'twitter:image', content: seoImage },
             ],
+            link: [
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+                { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-chrome-192x192.png' },
+                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+                { rel: 'manifest', href: '/site.webmanifest' },
+                { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#93c045' },
+            ]
         }
     },
     modules: ['@nuxtjs/apollo'],
